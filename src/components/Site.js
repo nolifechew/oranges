ad3e6765f74acb0f76c4307cf838f23d577d75a1
nolifechew <haunterkirby@gmail.com>
2022-01-22 11:17:52 -0500

    update

--- src/components/Site.js
+++ src/components/Site.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
+import logo from "../images/logo.png";
+
+import picnic from "../images/picnic.png";
+
 import no from "../images/no.png";
 
-import logo from "../images/logo.png";
+import juice from "../images/juice.png";
+
 import opensea from "../images/opensea.png";
 import twitter from "../images/twitter.png";
 
@@ -18,24 +23,44 @@ class Site extends Component {
 	<header>
 
 		{/* BUTTONS THAT SCROLL TO SECTION - ABOUT - ROADMAP */}
+		<div class="banner">
+
+			<h2>TWITTER</h2>
+			<h2>OPENSEA</h2>
+			<h2>CONNECT</h2>
+
+
+		</div>
 
 		<img src={logo} alt="" class="logo"/>
-
-		<div class="hill"></div>
-
-
 	</header>
 
 	{/* MAIN	----------------------------------------------------------------------------------------------------- */}
 	<main>
 
 		{/* ABOUT US */}
-		<div class="section aboutus">
+		<div class="section">
 
-			<h2>What is ORANGEFRIENDS?</h2>
+			<div class="hill"></div>
 
-			<p>Each orange originally started as seed, being planted naturally in the rich soil of orangum. Through the years rainfall and sunny days came along till the tree grew strong and tall. The oranges grew day by day till becoming a little orange. As they prematurely fall out, they’re looking for someone to foster.</p>
+			<div class="aboutus">
 
+				<div class="about-left">
+
+					<h1>about us</h1>
+
+					<p>Each orange originally started as seed, being planted naturally in the rich soil of orangum. Through the years rainfall and sunny days came along till the tree grew strong and tall. The oranges grew day by day till becoming a little orange. As they prematurely fall out, they’re looking for someone to foster.</p>
+
+				</div>
+
+				<div class="about-right">
+
+					<img src={picnic} alt="" class="picnic"/>
+
+				</div>
+
+			</div>
+			
 		</div>
 
 		{/* MINT */}
@@ -87,9 +112,19 @@ class Site extends Component {
 		{/* TRAITS */}
 		<div class="section traits">
 
-			<h1>TRAITS</h1>
+			<div class="traits-left">
 
-			<p>On a planet, lightyears away from Earth. A population of 10000 oranges are rolling around the planet oranguam waiting to be picked up to start it’s journey. You make the journey how you like it be, adventurous, exciting, thrilling, or just plain simple. That’s a decision you need to make.</p>
+				<h1>TRAITS</h1>
+
+				<p>With 10,000 oranges and X traits, every Orange Friend is unique. Some are zombies and some are made of ice. Some do buissness and some are in monkey costumes. Hopefully you get an Orange Friend with rare traits and can show them to your friends.</p>
+
+			</div>
+
+			<div class="traits-right">
+
+			</div>
+
+			
 
 		</div>
 
@@ -98,21 +133,32 @@ class Site extends Component {
 		{/* ROADMAP */}
 		<div class="section roadmap">
 
+			<div class="road-left">
+
+				<h1>ROADMAP</h1>
+
+				<h2 class="top-space">Phase 1: Events</h2>
+				<p>We will be hosting events for our holders such as ama’s, giveaways &amp; others events for us to interact. Provide updates and timelines of each phase launch. Allowing opportunities to be whitelisted and potentially a role in discord.</p>
+
+				<h2>Phase 2:  Mutations</h2>
+				<p>Currently Holders of oranges friends will be able to claim a mutation serum, where you can mutate your orange friend into a mutated orange. To claim a free serum you will need to own a minimum of 3 oranges friends or more. For every 3 oranges you can claim one serum. Whatever doesn’t get claimed will be up for a public mint. The supply of the serum will be 3333. The supply of the mutated orange friends will be limited to 3333 as well. </p>
+
+				<h2>Phase 3: Merchandising</h2>
+				<p>The aim is to launch a clothing line including hoodies, t-shirts, caps etc. Alongside accessories complementing your fit or just styling items such as keyring, lanyards, mugs etc. These will be both customizable to your nft or the standard logo of Orange friends.</p>
+
+				<h2>Phase 4: Voxel Art</h2>
+				<p>Voxel style little orange friends where it will be exclusive to people who hold the orange friend or mutated orange friend. There will be an alpha chat for the holders, allowing us to efficiently and effectively allocate the voxel orange to the current holders of the pixelated little orange friends. Will be buying land in the sandbox allowing you showcase and interact</p>
+				
+			</div>
+
+			<dic class="road-right">
+
+				<img src={juice} alt="" class="juice"/>
+
+			</dic>
+
 			<div class="timeline"></div>
-			<h1>ROADMAP</h1>
-
-			<h2 class="top-space">Phase 1: Events</h2>
-			<p>We will be hosting events for our holders such as ama’s, giveaways &amp; others events for us to interact. Provide updates and timelines of each phase launch. Allowing opportunities to be whitelisted and potentially a role in discord.</p>
-
-			<h2>Phase 2:  Mutations</h2>
-			<p>Currently Holders of oranges friends will be able to claim a mutation serum, where you can mutate your orange friend into a mutated orange. To claim a free serum you will need to own a minimum of 3 oranges friends or more. For every 3 oranges you can claim one serum. Whatever doesn’t get claimed will be up for a public mint. The supply of the serum will be 3333. The supply of the mutated orange friends will be limited to 3333 as well. </p>
-
-			<h2>Phase 3: Merchandising</h2>
-			<p>The aim is to launch a clothing line including hoodies, t-shirts, caps etc. Alongside accessories complementing your fit or just styling items such as keyring, lanyards, mugs etc. These will be both customizable to your nft or the standard logo of Orange friends.</p>
-
-			<h2>Phase 4: Voxel Art</h2>
-			<p>Voxel style little 
-				orange friends where it will be exclusive to people who hold the orange friend or mutated orange friend. There will be an alpha chat for the holders, allowing us to efficiently and effectively allocate the voxel orange to the current holders of the pixelated little orange friends. Will be buying land in the sandbox allowing you showcase and interact</p>
+			
 
 		</div>
 
@@ -124,7 +170,7 @@ class Site extends Component {
 			<div class="members">
 				<div class="member">
 					<div class="dot red"></div>
-					HUMPTY / LEAD
+					HUMPTY / LEAD and COMMUNITY
 
 				</div>
 				<div class="member">
@@ -134,7 +180,7 @@ class Site extends Component {
 				</div>
 				<div class="member">
 					<div class="dot green"></div>
-					KING / CONTRACT
+					ORANGEDEV / CONTRACT
 
 				</div>
 				<div class="member">
